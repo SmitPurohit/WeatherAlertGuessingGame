63d81a78a8eba7a6962aff7116aad071d6f4c428
Smit Purohit <purohitsmit@hotmail.com>
2025-04-15 20:57:46 -0400

    Add vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Game from './components/Game';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Game />
+      <Analytics />
     </div>
   );
 }
