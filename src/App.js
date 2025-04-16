f06c12b520bb25747863c8fb9b72720e6e9fb37b
Smit Purohit <purohitsmit@hotmail.com>
2025-04-15 21:05:57 -0400

    nvm no analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Game from './components/Game';
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Game />
-      <Analytics />
     </div>
   );
 }
